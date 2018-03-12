f86b326de8a93851c270e2414e50e27759da2524
Oles Kovtunets <olesoles@i.ua>
2018-03-12 02:26:21 +0200

    back to orig

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root  'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
-  root_path -> '/'
-  root_url  -> 'http://localhost:3000/'
 end
