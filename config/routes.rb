ae10a352b8ce09b82557518842666f53a6c1049c
Oles Kovtunets <olesoles@i.ua>
2018-03-12 02:19:27 +0200

    testing homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  root_path -> '/'
+  root_url  -> 'http://localhost:3000/'
 end
